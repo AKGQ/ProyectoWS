dff1bb773e6a77b20907db72d96f9e209c700a3c
Kevin <akevinngq@gmail.com>
2022-11-23 20:57:27 -0700

    X

---
+++ proyectows/README.txt
@@ -0,0 +1,6 @@
+INSTRUCCIONES PARA EJECUTAR EL PROYECTO
+
+pip3 install virtualenv
+virtualenv virtual
+pip3 install flask
+pip3 install Flask-PyMongo
---
+++ proyectows/src/app.py
@@ -0,0 +1,89 @@
+from flask import Flask, jsonify, request, Response, render_template, redirect
+from flask_pymongo import PyMongo
+from bson import json_util
+
+app = Flask(__name__)
+app.config["MONGO_URI"]='mongodb+srv://sysdi_user:8AHTInspUUlOFLjv@cluster0.tcufg.mongodb.net/sys_distribuidos?retryWrites=true&w=majority'
+mongo = PyMongo(app)
+
+@app.route('/api/pagos', methods=['POST']) 
+def crear():
+    #Recibir datos
+    numeroMatricula = request.json['NumeroMatricula']
+    tipoPago = request.json['TipoPago']
+    institucion = request.json['Institucion']
+    descripcion = request.json['Descripcion']
+    nombre = request.json['Nombre']
+    apellidos = request.json['Apellidos']
+    fecha = request.json['Fecha']
+
+    if numeroMatricula and tipoPago and institucion and nombre and apellidos:
+        mongo.db.Pagos.insert_one(
+            {'NumeroMatricula': numeroMatricula, 'TipoPago': tipoPago, 'Institucion': institucion, 'Descripcion': descripcion, 'Nombre': nombre, 'Apellidos': apellidos, 'Fecha': fecha}
+        )
+        respuesta = {
+            'NumeroMatricula': numeroMatricula,
+            'TipoPago': tipoPago,
+            'Institucion': institucion,
+            'Descripcion': descripcion,
+            'Nombre': nombre,
+            'Apellidos': apellidos,
+            'Fecha': fecha
+        }
+        return jsonify(respuesta)
+    else:
+        return error_registro()
+
+
+@app.route('/api/pagos/<numeroMatricula>', methods=['GET']) 
+def mostrar_usuario_matricula(numeroMatricula):
+    usuario = mongo.db.Pagos.find({'NumeroMatricula': numeroMatricula})
+    response = json_util.dumps(usuario)
+    if response != "[]":
+        return response(
+            response,
+            status=200,
+            mimetype='application/json'
+        )
+    else:
+        return no_encontrado()
+
+
+@app.route('/api/estudiantes/registrarcalificacion', methods=['GET', 'POST'])
+def registrarcalificacion():
+    if request.method == 'POST':
+        matricula = mongo.db.Pagos.find({'NumeroMatricula' : request.form['numeroMatricula']})
+        respuesta = json_util.dumps(matricula)
+        
+        if respuesta != "[]":
+            mongo.db.Estudiante.insert_one(
+                {'NumeroMatricula': request.form['numeroMatricula'], 'Materia': request.form['materia'], 'Calificacion': request.form['calificacion']}
+            )
+        else:
+            return no_encontrado()
+
+    return render_template('./templates/index.html')
+
+
+#MANEJO DE ERRORES
+@app.errorhandler(400) 
+def error_registro(error=None):
+    mensajeError = jsonify({
+        'mensajeError': 'Error al registrar: ' + request.url,
+        'status': 400
+    })
+    mensajeError.status_code = 400
+    return mensajeError
+
+
+@app.errorhandler(404) 
+def no_encontrado(error=None):
+    mensajeError = jsonify({
+        'mensajeError': 'Recurso no encontrado: ' + request.url,
+        'status': 404
+    })
+    mensajeError.status_code = 404
+    return mensajeError
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0')
